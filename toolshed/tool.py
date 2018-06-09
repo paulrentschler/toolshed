5823970297fb2f07215869e912718804837e31f4
Unknown <paul@rentschler.ws>
2018-06-08 22:14:32 -0500

    Swap the order of the write arguments

--- toolshed/tool.py
+++ toolshed/tool.py
@@ -5,7 +5,7 @@ class Tool(object):
     verbosity = 1
 
 
-    def write(self, msg, verbosity=1, end='\n'):
+    def write(self, msg, end='\n', verbosity=1):
         """Output text to standard output
 
         `msg` is only output if `self.verbosity` is greater than or equal
@@ -15,10 +15,11 @@ class Tool(object):
             msg {string} -- Message to be output
 
         Keyword Arguments:
-            verbosity {number} -- Verbosity level necessary to output the
-                                  message (default: {1})
             end {string} -- String ending character to be used rather than
                             a carriage return (default: {'\n'})
+            verbosity {integer} -- What verbosity level the tool must
+                                   be running at for the message to be
+                                   output (default: {1})
         """
         if self.verbosity >= verbosity:
             print(msg, end=end)
